1f5aa39c1b4b2df7c9686ea7c5441cc64d63e8c3
David Sena <sena.ufc@gmail.com>
2014-08-14 20:02:10 -0300

    dds

---
+++ aulas/aula08_14.08/dds.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+enum DiadaSemana{SEG, TER, QUA, QUI, SEX, SAB, DOM };
+
+bool eh_final_de_semana(DiadaSemana dia){
+    if(dia == SAB or dia == DOM)
+        return true;
+    return false;
+}
+
+int main ()
+{
+    cout << (eh_final_de_semana(DOM) == true);
+    cout << (eh_final_de_semana(SEX) == false);
+    return 0;
+}
--- aulas/aula08_14.08/par_impar.cpp
+++ aulas/aula08_14.08/par_impar.cpp
@@ -5,7 +5,12 @@ enum Opcao {PAR, IMPAR};
 enum Resultado {JOGADOR1, JOGADOR2};
 
 Resultado quem_ganhou( Opcao jog1, int valor_jog1, int valor_jog2){
+    int soma = valor_jog1 + valor_jog2;
 
+    if(((soma % 2 == 0) and jog1 == PAR) or
+       ((soma % 2 != 0) and jog1 == IMPAR))
+        return JOGADOR1;
+    return JOGADOR2;
 }
 
 
