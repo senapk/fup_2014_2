854e8f0a6ad8deb2b63292a05ae50aa2a877d8ad
David Sena <sena.ufc@gmail.com>
2014-08-19 19:58:46 -0300

    aula 9

---
+++ aulas/aula09_19.08/bib.hpp
@@ -0,0 +1,12 @@
+
+
+#ifndef BIB_HPP
+#define BIB_HPP
+
+#include <cmath>
+
+bool compara_float(float A, float B){
+    return (fabs(A - B) < 0.0000001);
+}
+
+#endif
---
+++ aulas/aula09_19.08/sapatos.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "bib.hpp"
+#include "bib.hpp"
+using namespace std;
+
+int main ()
+{
+    cout << compara_float((85.0/100.0 * 20.0), 17.0);
+    return 0;
+}
