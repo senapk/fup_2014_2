4c41c612e93956e08532b152e54b9c8dc042bddc
David Sena <sena.ufc@gmail.com>
2014-09-11 19:04:37 -0300

    prova

---
+++ provas/prova3/prova.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+vector<int> pegar_sozinhos(vector<int> vet){
+
+
+}
+
+vector<int> pegar_exemplares(vector<int> vet){
+
+}
+
+int main ()
+{
+    return 0;
+}
